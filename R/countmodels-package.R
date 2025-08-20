3c256fce153fdaeb5b712cfbb239d8da3f7063a6
marcio batista <marcioavbatista@gmail.com>
2025-08-20 16:19:31 -0300

    initial commit2

---
+++ R/countmodels-package.R
@@ -0,0 +1,18 @@
+#' The 'countmodels' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name countmodels-package
+#' @aliases countmodels
+#' @useDynLib countmodels, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.36.0.9000. https://mc-stan.org
+#'
+NULL
